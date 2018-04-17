5c086e087a55719e707eb738c5c9f69a31ecad9a
João Cerqueira <und0ck3d@protonmail.com>
2018-04-17 02:08:55 +0100

    bumped deps versions

--- mix.exs
+++ mix.exs
@@ -31,11 +31,11 @@ defmodule ElixirIpfsApi.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:httpoison, "~> 0.8.0"},
-      {:poison, "~> 2.0"},
+      {:httpoison, "~> 1.0"},
+      {:poison, "~> 3.1", override: true},
       {:inch_ex, "~> 0.5", only: :docs},
-      {:ex_doc, "~> 0.10.0", only: :dev},
-      {:earmark, "~> 0.1", only: :dev}
+      {:ex_doc, "~> 0.18.3", only: :dev},
+      {:earmark, "~> 1.2", only: :dev}
     ]
   end
 
